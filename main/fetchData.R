737ec803e3cbe3db072179c019c73e8f7d26a43d
grdphilip <philiprettig11@gmail.com>
2022-04-13 06:50:01 +0200

    Latest update

--- main/fetchData.R
+++ main/fetchData.R
@@ -6,7 +6,15 @@ library(fplr)
 
 url = "https://www.transfermarkt.co.uk/transfers/transferrekorde/statistik/top/plus/0/g"
 
-players <- fpl_get_player_all()
+
+players <- na.omit(fpl_get_player_all())
+
+for (i in players) {
+  if (players[i] == is.null) {
+    i <- "-"
+  }
+}
+
 nextGW <- fpl_get_gameweek_next()
 
 fpl_get_player_all()
@@ -18,19 +26,14 @@ playerwithID <- cbind.data.frame(players$id,players$second_name,players$team)
 #Returns the data of a single player
   search_player <- function(name) {
     tempIndex = which(players$second_name == name)
-    #Transpose matrix 
-    return(View(t(players[tempIndex,])))
+    #Transpose matrix for readability (t)
     
+    return(View(t(players[tempIndex,])))
   }
   
+  View(players[order(players$ep_next)])
+  
   search_player("Mount")
-  
-  
-  which(players$second_name == "Saka")
-
-  search_player("Salah","Saka")
-
-  #Get SALAH historic data
   
   View(fpl_get_player_detailed(player_id = 233))
   
--- main/pythonData.py
+++ main/pythonData.py
@@ -1,1 +1,9 @@
-x = 10
+import pandas as pd
+from pulp import *
+import requests
+
+url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+
+r = requests.get(url)
+json = r.json()
+elements_df = pd.DataFrame(json['elements'])
